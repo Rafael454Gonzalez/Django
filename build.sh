8bb3a722ed2325018a70c7b3964ad590bc5af7f9
Rafael <rokero.0407@hotmail.com>
2023-09-20 17:29:18 -0500

    build.sh test

--- build.sh
+++ build.sh
@@ -9,6 +9,3 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 python manage.py createsuperuser --no-input
-export DJANGO_SUPERUSER_EMAIL = <rokero.0407@hotmail.com>
-export DJANGO_SUPERUSER_PASSWORD = <04072003>
-export DJANGO_SUPERUSER_USERNAME =<username>
