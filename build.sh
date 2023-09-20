c2d47bde27a9b2c7cdbc50b2414e390a17a56a5b
Rafael <rokero.0407@hotmail.com>
2023-09-20 16:44:02 -0500

    build.sh test

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuse
+python manage.py createsuperuser
 if [[ $CREATE_SUPERUSER ]];
 then
   python world_champ_2022/manage.py createsuperuser --no-input
