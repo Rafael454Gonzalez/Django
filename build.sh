0dd82c49beaf1e557b297810af969ecac5f1ed16
Rafael <rokero.0407@hotmail.com>
2023-09-20 17:55:41 -0500

    build.sh test

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-python manage.py createsuperuser --username USERNAME --email EMAIL --no-input 
+python manage.py createsuperuser --username USERNAME --email rokero.0407@hotmail.com --no-input 
