aac2e5a28cac5b7aabc9f258e81f5115a7d43416
rafelgonzalez <rokero.0407@hotmail.com>
2023-10-05 10:52:37 -0500

    build.sh test

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@
 set -o errexit
 
 #poetry install
-#poetry install
-#poetry install
+
+
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-DJANGO_SUPERUSER_PASSWORD=0407 python manage.py createsuperuser --no-input --username username  --email rokero.0407@hotmail.com
+#DJANGO_SUPERUSER_PASSWORD=0407 python manage.py createsuperuser --no-input --username username  --email rokero.0407@hotmail.com
