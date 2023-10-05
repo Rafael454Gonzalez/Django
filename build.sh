4872dd164560d17228456a168c4e124c33d8a3b8
rafelgonzalez <rokero.0407@hotmail.com>
2023-10-05 09:57:53 -0500

    hola

--- build.sh
+++ build.sh
@@ -10,4 +10,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-#DJANGO_SUPERUSER_PASSWORD=04072003Ra python manage.py createsuperuser --no-input --username Rafael3  --email rokero.0407@hotmail.com
+DJANGO_SUPERUSER_PASSWORD=04072003Ra python manage.py createsuperuser --no-input --username Rafael3  --email rokero.0407@hotmail.com
